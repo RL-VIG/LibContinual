51a74efb083005a714f8fec98429c1eee70a2eb9
myscius <1316099740@qq.com>
2023-09-22 14:43:08 +0800

    LWF

--- run_trainer.py
+++ run_trainer.py
@@ -6,14 +6,18 @@ import torch
 import os
 from core.config import Config
 from core import Trainer
+import time
 
 def main(rank, config):
+    begin = time.time()
     trainer = Trainer(rank, config)
     trainer.train_loop()
+    print("Time cost : ",time.time()-begin)
 
 if __name__ == "__main__":
     # config = Config("./config/finetune.yaml").get_config_dict()
     config = Config("./config/lucir.yaml").get_config_dict()
+    # config = Config("./config/lwf.yaml").get_config_dict()
 
     if config["n_gpu"] > 1:
         pass
