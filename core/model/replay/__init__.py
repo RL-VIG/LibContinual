31f804b9b252da315042580f7fb1ff6227843be6
yizhibaiwuya <2478192663@qq.com>
2023-09-27 14:39:02 +0800

    remove redundant comments

--- core/model/replay/__init__.py
+++ core/model/replay/__init__.py
@@ -1,6 +1,6 @@
 from .finetune import Finetune
 from .icarl import ICarl
 from .lucir import LUCIR
-from .lwf import LWF  #先导入这个文件，才能与finetune成为平级包文件
+from .lwf import LWF
 from .wa import WA
 
--- run_trainer.py
+++ run_trainer.py
@@ -15,9 +15,7 @@ def main(rank, config):
     print("Time cost : ",time.time()-begin)
 
 if __name__ == "__main__":
-    # config = Config("./config/finetune.yaml").get_config_dict()
     config = Config("./config/lucir.yaml").get_config_dict()
-    # config = Config("./config/lwf.yaml").get_config_dict()
 
     if config["n_gpu"] > 1:
         pass
