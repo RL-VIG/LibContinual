ecdcb4a91c6feca4cadf39a392abae40ccc7d576
yizhibaiwuya <2478192663@qq.com>
2023-11-11 23:15:06 +0800

    add resnet32

--- core/model/backbone/resnet.py
+++ core/model/backbone/resnet.py
@@ -10,6 +10,8 @@ try:
 except:
     from torch.hub import load_state_dict_from_url
 
+from torch.nn.parameter import Parameter
+import torch.nn.functional as F
 
 
 # __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
@@ -317,78 +319,78 @@ def resnet152(pretrained=False, progress=True, **kwargs):
 
 
 
-class CifarResNet(nn.Module):
-    """
-    ResNet optimized for the Cifar Dataset, as specified in
-    https://arxiv.org/abs/1512.03385.pdf
-    """
+# class CifarResNet(nn.Module):
+#     """
+#     ResNet optimized for the Cifar Dataset, as specified in
+#     https://arxiv.org/abs/1512.03385.pdf
+#     """
 
-    def __init__(self, block, depth, channels=3):
-        super(CifarResNet, self).__init__()
+#     def __init__(self, block, depth, channels=3):
+#         super(CifarResNet, self).__init__()
 
-        # Model type specifies number of layers for CIFAR-10 and CIFAR-100 model
-        assert (depth - 2) % 6 == 0, 'depth should be one of 20, 32, 44, 56, 110'
-        layer_blocks = (depth - 2) // 6
+#         # Model type specifies number of layers for CIFAR-10 and CIFAR-100 model
+#         assert (depth - 2) % 6 == 0, 'depth should be one of 20, 32, 44, 56, 110'
+#         layer_blocks = (depth - 2) // 6
 
-        self.conv_1_3x3 = nn.Conv2d(channels, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(16)
+#         self.conv_1_3x3 = nn.Conv2d(channels, 16, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.bn_1 = nn.BatchNorm2d(16)
 
-        self.inplanes = 16
-        self.stage_1 = self._make_layer(block, 16, layer_blocks, 1)
-        self.stage_2 = self._make_layer(block, 32, layer_blocks, 2)
-        self.stage_3 = self._make_layer(block, 64, layer_blocks, 2)
-        self.avgpool = nn.AvgPool2d(8)
-        self.out_dim = 64 * block.expansion
-        self.fc = nn.Linear(64*block.expansion, 10)
+#         self.inplanes = 16
+#         self.stage_1 = self._make_layer(block, 16, layer_blocks, 1)
+#         self.stage_2 = self._make_layer(block, 32, layer_blocks, 2)
+#         self.stage_3 = self._make_layer(block, 64, layer_blocks, 2)
+#         self.avgpool = nn.AvgPool2d(8)
+#         self.out_dim = 64 * block.expansion
+#         self.fc = nn.Linear(64*block.expansion, 10)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                # m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight)
-                m.bias.data.zero_()
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+#                 m.weight.data.normal_(0, math.sqrt(2. / n))
+#                 # m.bias.data.zero_()
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.kaiming_normal_(m.weight)
+#                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-            )
-            # downsample = DownsampleA(self.inplanes, planes * block.expansion, stride)
+#     def _make_layer(self, block, planes, blocks, stride=1):
+#         downsample = None
+#         if stride != 1 or self.inplanes != planes * block.expansion:
+#             downsample = nn.Sequential(
+#                 nn.Conv2d(self.inplanes, planes * block.expansion,
+#                           kernel_size=1, stride=stride, bias=False),
+#             )
+#             # downsample = DownsampleA(self.inplanes, planes * block.expansion, stride)
 
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+#         layers = []
+#         layers.append(block(self.inplanes, planes, stride, downsample))
+#         self.inplanes = planes * block.expansion
+#         for i in range(1, blocks):
+#             layers.append(block(self.inplanes, planes))
 
-        return nn.Sequential(*layers)
+#         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        x = self.conv_1_3x3(x)  # [bs, 16, 32, 32]
-        x = F.relu(self.bn_1(x), inplace=True)
+#     def forward(self, x):
+#         x = self.conv_1_3x3(x)  # [bs, 16, 32, 32]
+#         x = F.relu(self.bn_1(x), inplace=True)
 
-        x_1 = self.stage_1(x)  # [bs, 16, 32, 32]
-        x_2 = self.stage_2(x_1)  # [bs, 32, 16, 16]
-        x_3 = self.stage_3(x_2)  # [bs, 64, 8, 8]
+#         x_1 = self.stage_1(x)  # [bs, 16, 32, 32]
+#         x_2 = self.stage_2(x_1)  # [bs, 32, 16, 16]
+#         x_3 = self.stage_3(x_2)  # [bs, 64, 8, 8]
 
-        pooled = self.avgpool(x_3)  # [bs, 64, 1, 1]
-        features = pooled.view(pooled.size(0), -1)  # [bs, 64]
+#         pooled = self.avgpool(x_3)  # [bs, 64, 1, 1]
+#         features = pooled.view(pooled.size(0), -1)  # [bs, 64]
 
-        return {
-            'fmaps': [x_1, x_2, x_3],
-            'features': features
-        }
+#         return {
+#             'fmaps': [x_1, x_2, x_3],
+#             'features': features
+#         }
 
-    @property
-    def last_conv(self):
-        return self.stage_3[-1].conv_b
+#     @property
+#     def last_conv(self):
+#         return self.stage_3[-1].conv_b
 
 
 def cifarresnet(**kwargs):
@@ -405,3 +407,171 @@ class BiasLayer(nn.Module):
         return self.alpha * x + self.beta
     def printParam(self, i):
         print(i, self.alpha.item(), self.beta.item())
+
+
+
+
+
+
+class CosineLinear(nn.Module):
+    def __init__(self, in_features, out_features, sigma=True):
+        super(CosineLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if sigma:
+            self.sigma = Parameter(torch.Tensor(1))
+        else:
+            self.register_parameter('sigma', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.sigma is not None:
+            self.sigma.data.fill_(1) #for initializaiton of sigma
+
+    def forward(self, input):
+        #w_norm = self.weight.data.norm(dim=1, keepdim=True)
+        #w_norm = w_norm.expand_as(self.weight).add_(self.epsilon)
+        #x_norm = input.data.norm(dim=1, keepdim=True)
+        #x_norm = x_norm.expand_as(input).add_(self.epsilon)
+        #w = self.weight.div(w_norm)
+        #x = input.div(x_norm)
+        
+        out = F.linear(F.normalize(input, p=2,dim=1), \
+                F.normalize(self.weight, p=2, dim=1))
+        if self.sigma is not None:
+            out = self.sigma * out
+        return out
+
+class SplitCosineLinear(nn.Module):
+    #consists of two fc layers and concatenate their outputs
+    def __init__(self, in_features, out_features1, out_features2, sigma=True):
+        super(SplitCosineLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features1 + out_features2
+        self.fc1 = CosineLinear(in_features, out_features1, False)
+        self.fc2 = CosineLinear(in_features, out_features2, False)
+        if sigma:
+            self.sigma = Parameter(torch.Tensor(1))
+            self.sigma.data.fill_(1)
+        else:
+            self.register_parameter('sigma', None)
+
+    def forward(self, x):
+        out1 = self.fc1(x)
+        out2 = self.fc2(x)
+        out = torch.cat((out1, out2), dim=1) #concatenate along the channel
+        if self.sigma is not None:
+            out = self.sigma * out
+        return out
+
+
+
+
+
+
+
+class modified_BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, last=False):
+        super(modified_BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.last = last
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        if not self.last: #remove ReLU in the last layer
+            out = self.relu(out)
+
+        return out
+
+class modified_ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=50):
+        self.inplanes = 16
+        super(modified_ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2, last_phase=True)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        # self.fc = modified_linear.CosineLinear(64 * block.expansion, num_classes)
+        self.fc = CosineLinear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, last_phase=False):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        if last_phase:
+            for i in range(1, blocks-1):
+                layers.append(block(self.inplanes, planes))
+            layers.append(block(self.inplanes, planes, last=True))
+        else: 
+            for i in range(1, blocks):
+                layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def resnet20(pretrained=False, **kwargs):
+    n = 3
+    model = modified_ResNet(modified_BasicBlock, [n, n, n], num_classes=50)
+    return model
+
+def resnet32(pretrained=False, **kwargs):
+    n = 5
+    model = modified_ResNet(modified_BasicBlock, [n, n, n], num_classes=50)
+    return model
