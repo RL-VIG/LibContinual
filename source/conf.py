9a2a6f4be250ab004ef665657614e047ad1cf557
yizhibaiwuya <2478192663@qq.com>
2024-04-23 11:52:37 +0800

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,9 @@ release = '0.0.1-alpha'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["recommonmark", 'sphinx_markdown_tables']
+extensions = ['myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",]
 
 templates_path = ['_templates']
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
